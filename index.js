8eb77729a5edb86e1ec5abd0384f920fdf3c229b
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-09 02:59:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
-}
+  var orderedList = []
+  for (var i = 0; i < dwarves.length; i++){
+  orderedList.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return orderedList.join("")
+  }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newList = [];
+  for (var i = 0; i < planeteerCalls.length; i++){
+    newList.push(`${planeteerCalls[i]}`.toUpperCase() + "!");
+  }
+  return newList;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+  	if (words[i].length > 4) {
+  		return true;
+  	} else {
+  		return false;
+  	}
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
